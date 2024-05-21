492c5b090dd756877779e3547d169df71c41a5f0
joelSU13 <166922447+joelSU13@users.noreply.github.com>
2024-05-21 18:12:13 +0000

    certo

--- src/main/java/competec/aula3_arranjos/Exercicio2.java
+++ src/main/java/competec/aula3_arranjos/Exercicio2.java
@@ -3,15 +3,30 @@ package competec.aula3_arranjos;
 import java.util.Scanner;
 
 public class Exercicio2 {
-    System.out.println("Digite a quantidade de numeros no arranjo: ");
-    num1 = leia.nextInt();
-        for(int i = 0;i < num1;i++) {
-            System.out.print("Digite o primeiro número do seu arranjo: ");
-            arj = leia.nextInt();
-            int num2 [] = new int [arj];
-            System.out.println("O arranjo tem os seguintes numeros: " + num2.length()+ " " +num2[]);
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int arj;
+        double media,num1=1,num3=0;
+    
+        System.out.println("Digite a quantidade de números no arranjo: ");
+        arj = leia.nextInt();
+        double num2[] = new double[arj];
+    
+        for (int i = 0; i < arj; i++) {
+            System.out.print("Digite o número " + (i + 1) + " do seu arranjo: ");
+            num2[i] = leia.nextInt();
         }
-        System.out.println("A quantidade de numeros no arranjo é: " + num1);
+    
+        System.out.println("O arranjo tem os seguintes números:");
+        for (int i = 0; i < arj; i++) {
+            System.out.print(num2[i] + " ");
+            num1 = num2[i]*num1;
+            num3 = num2[i]+num3;
         }
-	}
+        media = num3/arj;
+        System.out.println("\nA média do arranjo é: " + media);
+        System.out.println("\nA quantidade de números no arranjo é: " + arj);
+        System.out.println("\nA multiplicação de todos os números do arranjo é igual é: " + num1);
+        }
+    }
 
